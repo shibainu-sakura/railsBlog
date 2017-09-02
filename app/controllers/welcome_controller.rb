f4db0c4ddf6bfe6b204407c03084877163defb2c
Takahiro Suzuki <takahirosuzuki.k010@icloud.com>
2017-09-02 14:52:37 +1000

    initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  def index
+    @greetings = "Hello"
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  root 'welcome#index'
+
+  resources :articles
+
+end 
